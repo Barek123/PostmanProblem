1acd2c646dd9cdd7cc3c17c3e1de430e3880572e
Bartek <marcb9301@gmail.com>
2017-12-21 21:11:39 +0100

    resutl 100.0

--- src/main/java/pl/marcb/postman/algorithm/Cpp.java
+++ src/main/java/pl/marcb/postman/algorithm/Cpp.java
@@ -30,32 +30,28 @@ public class Cpp {
         boolean allEdges = false;
         int index = 0;
 
-        do{
-            do {
-                CppAlgorithm cppAlgorithm = constructCppAlgorithm();
-                cppAlgorithm.evolve();
+        do {
+            CppAlgorithm cppAlgorithm = constructCppAlgorithm();
+            cppAlgorithm.evolve();
 
-                List<String> genes = new LinkedList<String>(Arrays.asList(cppAlgorithm.getFittestChromosome().toString().split("\\|")));
-                genes.add(genes.get(0));
+            List<String> genes = new LinkedList<String>(Arrays.asList(cppAlgorithm.getFittestChromosome().toString().split("\\|")));
+            genes.add(genes.get(0));
 
-                allEdges = cppUtils.allPointsExist(genes);
-                if (allEdges) {
-                    resultDTO.setFittestChromosome(cppAlgorithm.getFittestChromosome());
-                    resultDTO.setFittestChromosomesFitness(cppAlgorithm.getFittestChromosomesFitness());
-                    resultDTO.setFoundFittestChromoseme(true);
+            allEdges = cppUtils.allPointsExist(genes);
+            if (allEdges) {
+                resultDTO.setFittestChromosome(cppAlgorithm.getFittestChromosome());
+                resultDTO.setFittestChromosomesFitness(cppAlgorithm.getFittestChromosomesFitness());
+                resultDTO.setFoundFittestChromoseme(true);
 
-                    System.out.print("Znaleziono prawidłową śceżkę  o liczbie krawędzi "+(genes.size()-1)+" - "+ String.join("-", genes));
-                    System.out.println(" o wartości " + cppAlgorithm.getFittestChromosomesFitness());
-                } else {
-                    resultDTO.setFoundFittestChromoseme(false);
+                System.out.print("Znaleziono prawidłową śceżkę  o liczbie krawędzi " + (genes.size() - 1) + " - " + String.join("-", genes));
+                System.out.println(" o wartości " + cppAlgorithm.getFittestChromosomesFitness());
+            } else {
+                resultDTO.setFoundFittestChromoseme(false);
 
-                    System.out.println("Nie udało się znaleźć połączenia");
-                }
-                index++;
-            }while (!allEdges && index<maxNumExecutions);
-            edge++;
-            index=0;
-        }while(!resultDTO.isFoundFittestChromoseme());
+                System.out.println("Nie udało się znaleźć połączenia");
+            }
+            index++;
+        } while (!allEdges && index < maxNumExecutions);
     }
 
     public ResultDTO getResultDTO() {
@@ -65,16 +61,16 @@ public class Cpp {
     private CppAlgorithm constructCppAlgorithm() throws GAException {   //build from config
         ConfigProperties configProperties = ConfigProperties.getInstance();
 
-        int populationDim=Integer.parseInt(configProperties.getProperties("config.populationDim"));
-        double crossoverProb=Double.parseDouble(configProperties.getProperties("config.crossoverProb"));
-        int randomSelectionChance=Integer.parseInt(configProperties.getProperties("config.randomSelectionChance"));
-        int maxGenerations=Integer.parseInt(configProperties.getProperties("config.maxGenerations"));
-        int numPrelimRuns=Integer.parseInt(configProperties.getProperties("config.numPrelimRuns"));
-        int maxPrelimGenerations=Integer.parseInt(configProperties.getProperties("config.maxPrelimGenerations"));
-        double mutationProb=Double.parseDouble(configProperties.getProperties("config.mutationProb"));
-        int chromDecPts=Integer.parseInt(configProperties.getProperties("config.chromDecPts"));
-        int crossoverType=Integer.parseInt(configProperties.getProperties("config.crossoverType"));
-        boolean computeStatistics= Boolean.parseBoolean(configProperties.getProperties("config.computeStatistics"));
+        int populationDim = Integer.parseInt(configProperties.getProperties("config.populationDim"));
+        double crossoverProb = Double.parseDouble(configProperties.getProperties("config.crossoverProb"));
+        int randomSelectionChance = Integer.parseInt(configProperties.getProperties("config.randomSelectionChance"));
+        int maxGenerations = Integer.parseInt(configProperties.getProperties("config.maxGenerations"));
+        int numPrelimRuns = Integer.parseInt(configProperties.getProperties("config.numPrelimRuns"));
+        int maxPrelimGenerations = Integer.parseInt(configProperties.getProperties("config.maxPrelimGenerations"));
+        double mutationProb = Double.parseDouble(configProperties.getProperties("config.mutationProb"));
+        int chromDecPts = Integer.parseInt(configProperties.getProperties("config.chromDecPts"));
+        int crossoverType = Integer.parseInt(configProperties.getProperties("config.crossoverType"));
+        boolean computeStatistics = Boolean.parseBoolean(configProperties.getProperties("config.computeStatistics"));
 
         return new CppAlgorithm(edge,
                 populationDim,
